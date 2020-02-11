bf66acf6d88f1203b77bd224d06aee7dad372ebb
TotaBraz <tota.braz@gmail.com>
2020-02-10 23:40:38 -0300

    Developing Progressive Web Apps 03.2: Fetch API

--- fetch-api-lab/app/js/main.js
+++ fetch-api-lab/app/js/main.js
@@ -24,12 +24,23 @@ function logError(error) {
   console.log('Looks like there was a problem:', error);
 }
 
+function validateResponse(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
 
 // Fetch JSON ----------
 
 function fetchJSON() {
-  // TODO
+  // fetch('examples/wrong-url.json')
+  fetch('examples/animals.json')
+    .then(validateResponse)
+    .then(logResult)
+    .catch(logError);
 }
+
 const jsonButton = document.getElementById('json-btn');
 jsonButton.addEventListener('click', fetchJSON);
 
@@ -37,17 +48,24 @@ jsonButton.addEventListener('click', fetchJSON);
 // Fetch Image ----------
 
 function fetchImage() {
-  // TODO
+  fetch('examples/fetching.jpg')
+    .then(validateResponse)
+    .then(readResponseAsBlob)
+    .then(showImage)
+    .catch(logError);
 }
+
 const imgButton = document.getElementById('img-btn');
 imgButton.addEventListener('click', fetchImage);
 
 
 // Fetch text ----------
 
-function fetchText() {
-  // TODO
+function fetchText(responseAsText) {
+  const msg = document.getElementById('message')
+  msg.textContent = responseAsText
 }
+
 const textButton = document.getElementById('text-btn');
 textButton.addEventListener('click', fetchText);
 
@@ -55,8 +73,15 @@ textButton.addEventListener('click', fetchText);
 // HEAD request ----------
 
 function headRequest() {
-  // TODO
+  fetch('examples/words.txt', {
+    method: 'HEAD'
+  })
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(logResult)
+    .catch(logError);
 }
+
 const headButton = document.getElementById('head-btn');
 headButton.addEventListener('click', headRequest);
 
@@ -65,7 +90,42 @@ headButton.addEventListener('click', headRequest);
 
 /* NOTE: Never send unencrypted user credentials in production! */
 function postRequest() {
-  // TODO
+  const formData = new FormData(document.getElementById('msg-form'));
+  const messageHeaders = new Headers({
+    'Content-Type': 'application/json',
+    'X-Custom': 'hello world',
+  })
+
+  fetch('http://localhost:5001/', {
+    method: 'POST',
+    headers:messageHeaders,
+    body: JSON.stringify({ lab: 'fetch', status: 'fun' }),
+    mode: 'no-cors'
+  })
+    .then(logResult)
+    .catch(logError);
 }
+
+
 const postButton = document.getElementById('post-btn');
 postButton.addEventListener('click', postRequest);
+
+
+
+
+const showImage = (responseAsBlob) => {
+  const container = document.getElementById('img-container')
+  const imgElem = document.createElement('img');
+  container.appendChild(imgElem);
+  const imgUrl = URL.createObjectURL(responseAsBlob);
+  imgElem.src = imgUrl;
+}
+
+
+const readResponseAsBlob = response => {
+  return response.blob()
+}
+
+function readResponseAsText(response) {
+  return response.text();
+}
