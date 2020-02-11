4a65605f516ff24203f1844c9891adeb0e022c29
Tota <hey@totabraz.com>
2020-02-11 14:50:11 -0300

    Developing Progressive Web Apps 04: Workbox

--- workbox-lab/project/gulpfile.js
+++ workbox-lab/project/gulpfile.js
@@ -13,26 +13,52 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+const workboxBuild = require('workbox-build');
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const del = require('del');
 
+const buildSw = () => {
+  return workboxBuild.injectManifest({
+    swSrc: 'src/sw.js',
+    swDest: 'build/sw.js',
+    globDirectory: 'build',
+    globPatterns: [
+      '**\/*.css',
+      'index.html',
+      'js\/animation.js',
+      'images\/home\/*.jpg',
+      'images\/icon\/*.svg',
+      'pages/offline.html',
+      'pages/404.html'
+    ]
+  }).then(resources => {
+    console.log(`Injected ${resources.count} resources for precaching, ` +
+      `totaling ${resources.size} bytes.`);
+  }).catch(err => {
+    console.log('Uh oh 😬', err);
+  });
+}
+gulp.task('build-sw', buildSw);
+
+
 // Clean "build" directory
 const clean = () => {
-  return del(['build/*'], {dot: true});
+  return del(['build/*'], { dot: true });
 };
 gulp.task('clean', clean);
 
 // Copy "src" directory to "build" directory
 const copy = () => {
- return gulp.src(['src/**/*']).pipe(gulp.dest('build'));
+  return gulp.src(['src/**/*']).pipe(gulp.dest('build'));
 };
 gulp.task('copy', copy);
 
 // TODO - create a task to build the service worker
 
 // This is the app's build process
-const build = gulp.series('clean', 'copy');
+const build = gulp.series('clean', 'copy', 'build-sw');
 gulp.task('build', build);
 
 // Build the app, run a local dev server, and rebuild on "src" file changes
--- workbox-lab/project/src/sw.js
+++ workbox-lab/project/src/sw.js
@@ -1,1 +1,75 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+
+    workbox.precaching.precacheAndRoute([]);
+
+    workbox.routing.registerRoute(
+        /(.*)articles(.*)\.(?:png|gif|jpg)/,
+        workbox.strategies.cacheFirst({
+            cacheName: 'images-cache',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                })
+            ]
+        })
+    );
+
+    const articleHandler = workbox.strategies.networkFirst({
+        cacheName: 'articles-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 50,
+            })
+        ]
+    });
+
+    workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+        return articleHandler.handle(args);
+    });
+
+    const articleHandler = workbox.strategies.networkFirst({
+        cacheName: 'articles-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 50,
+            })
+        ]
+    });
+
+    workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+        return articleHandler.handle(args)
+            .then(response => {
+                if (!response) {
+                    return caches.match('pages/offline.html');
+                } else if (response.status === 404) {
+                    return caches.match('pages/404.html');
+                }
+                return response;
+            });
+
+    });
+
+
+    workbox.routing.registerRoute(/(.*)pages\/post(.*)\.html/, args => {
+        console.log('test');
+        return postHandler.handle(args).then(response => {
+            if (response.status === 404) {
+                return caches.match('pages/404.html');
+            }
+            return response;
+        })
+            .catch(function () {
+                return caches.match('pages/offline.html');
+            });
+    });
+
+
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+
+}
 
